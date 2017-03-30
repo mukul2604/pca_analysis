8f99bba3381e82680541f9a610e931e86784bb37
mukul2604 <mukul312@gmail.com>
2017-03-29 22:17:32 -0400

    panda skmeans

--- apppanda.py
+++ apppanda.py
@@ -3,10 +3,13 @@ from flask import render_template
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
 from sklearn.manifold import Isomap
 from sklearn.manifold import MDS
 from sklearn.metrics.pairwise import cosine_similarity
@@ -16,24 +19,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    preprocessdata("data/Letter_recognition.csv", False)
     return render_template("index.html")
-    # return 'Hello'
-
 
 @app.route("/lr/details")
-def traffic_violation_details():
-    df = pd.read_csv("data/Letter_recognition.csv")
+def letter_recognition_details():
+    return "details"
+
+
+def preprocessdata(datapath, doplot):
+    df = pd.read_csv(datapath)
     encoder = preprocessing.LabelEncoder()
-    random_sample = df.take(np.random.permutation(len(df))[:5000])
-    random_sample_encoded =random_sample.apply(encoder.fit_transform)
+    random_sample = df.take(np.random.permutation(len(df))[:1000])
+    random_sample_encoded = random_sample.apply(encoder.fit_transform)
 
     x = np.array(random_sample_encoded)
     x = x.astype(int)
-    kmeans = KMeans(n_clusters=5, random_state=0).fit(x)
+    ks = range(1, 16)
+    kmeans = [KMeans(n_clusters=i, random_state=0) for i in ks]
+    score = [kmeans[i].fit(x).score(x) for i in range(len(kmeans))]
+    score = [-score[i] for i in range(len(ks))]
 
-    # return str(x)
-    return str(kmeans.labels_)
+    if doplot:
+        plt.plot(ks, score)
+        plt.ylabel('Objective function value')
+        plt.xlabel('Number of clusters')
+        plt.show()
+
+
+def main():
+    preprocessdata("data/Letter_recognition.csv", True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    main()
