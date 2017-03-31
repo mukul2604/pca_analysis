e564495321b518868dd8b7d230158f50a25e2e16
mukul2604 <mukul312@gmail.com>
2017-03-31 14:47:41 -0400

    second part started

--- apppanda.py
+++ apppanda.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.manifold import Isomap
 from sklearn.manifold import MDS
 from sklearn.metrics.pairwise import cosine_similarity
@@ -65,6 +67,12 @@ def stratified_sampling(kmean_obj, samples):
 
 def decimate_data(datapath, doplot):
     df = pd.read_csv(datapath)
+    df.columns = ["V" + str(i) for i in
+                    range(1, len(df.columns) + 1)]  # rename column names to be similar to R naming convention
+    df.V1 = df.V1.astype(str)
+    X = df.loc[:, "V2":]  # independent variables data
+    y = df.V1  # dependent variable data
+
     encoder = preprocessing.LabelEncoder()
     """
         Initial Random Sampling: takes 20% sample of total
@@ -72,7 +80,7 @@ def decimate_data(datapath, doplot):
     """
     pctg = 0.2
     sample_len = int(len(df) * pctg)
-    random_sample = df.take(np.random.permutation(len(df))[:sample_len])
+    random_sample = X.take(np.random.permutation(len(df))[:sample_len])
     random_sample_encoded = random_sample.apply(encoder.fit_transform)
 
     """
@@ -80,43 +88,53 @@ def decimate_data(datapath, doplot):
     """
     x = np.array(random_sample_encoded)
     x = x.astype(int)
-    ks = range(1, 16)
 
+    ks = range(1, 16)
     kmeans = [KMeans(n_clusters=i, random_state=0) for i in ks]
     score = [kmeans[i].fit(x).score(x) for i in range(len(kmeans))]
     score = [-score[i] for i in range(len(ks))]
 
     if doplot:
+        plt.suptitle("Elbow Plot", fontsize=14, fontweight='bold')
         plt.plot(ks, score)
-        plt.ylabel('Objective function value')
+        plt.ylabel('Objective Function Value')
         plt.xlabel('Number of clusters')
         plt.show()
 
-    """from plot elbow is found at k = 4
+    """
+        from elbow plot, elbow is found at k = 4
         next do the stratified sampling on those 4 clusters.
     """
     k_elbow = 4
 
     decimated_data = stratified_sampling(kmeans[k_elbow-1], x)
-    return decimated_data
+    #  return standardized data
+    return StandardScaler().fit_transform(decimated_data.astype(float))
 
 
-def dimension_reduction(datapath):
-    decimated_data = decimate_data(datapath, False)
+def dimension_reduction(datapath, draw_plots):
+    decimated_data = decimate_data(datapath, draw_plots)
     pca = PCA()
     pca.fit(decimated_data)
-    features = range(pca.n_components_)
+    components = range(pca.n_components_)
+    components = [x+1 for x in components]
 
-    plt.bar(features, pca.explained_variance_)
-    plt.scatter(features, np.cumsum(pca.explained_variance_))
-    plt.xticks(features)
-    plt.ylabel('variance')
-    plt.xlabel('PCA feature')
-    plt.show()
+    if draw_plots:
+        colors = np.random.rand(100)
+        plt.suptitle("Scree Plot", fontsize=14, fontweight='bold')
+        plt.scatter(components, pca.explained_variance_, c=colors, alpha=0.5)
+        plt.plot(components, pca.explained_variance_)
+        line_data = np.array([1 for i in xrange(len(components))])
+        plt.plot(components, line_data, 'r--')
+        plt.xticks([x for x in components])
+        plt.ylabel('Eigen Values')
+        plt.xlabel('PCA Components')
+        plt.show()
+    print pca.explained_variance_
 
 
 def main():
-    dimension_reduction("data/Letter_recognition.csv")
+    dimension_reduction("data/Letter_recognition.csv", True)
     # decimate_data("data/Letter_recognition.csv", False)
     # app.run(host='127.0.0.1', port=5000, debug=True)
 
