057235fa28b5edd08c1d530e56323c10831387b4
mukul2604 <mukul312@gmail.com>
2017-04-02 16:10:43 -0400

    js

---
+++ static/js/main.js
@@ -0,0 +1,157 @@
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var elements = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      if (element.classList.contains("show")) {
+        element.classList.remove("show");
+      }
+    }
+  }
+}
+
+function chartTypeFunction() {
+    document.getElementById("charttypes").classList.toggle("show");
+}
+
+
+function twoDScatterPlot(filename, id) {
+
+	var elements = document.getElementById("charttypes").children;
+	var i;
+
+	for (i=0; i < elements.length; i++) {
+		var element = elements[i];
+		if (id == element.id) {
+			var selectElem = document.getElementById(id);
+			d3.select(selectElem).attr("class", "selected");
+		} else {
+			var selectElem = document.getElementById(element.id);
+			d3.select(selectElem).attr("class", "dropdown");
+		}
+	}
+
+	filename = "./static/data/" + filename;
+
+	var margin = {top: 100, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+	// setup x
+	var xValue = function(d) { return d[0];}, // data -> value
+		xScale = d3.scaleLinear().range([0, width]), // value -> display
+		xMap = function(d) { return xScale(xValue(d));}, // data -> display
+		xAxis = d3.axisBottom(xScale);
+
+	// setup y
+	var yValue = function(d) { return d[1];}, // data -> value
+		yScale = d3.scaleLinear().range([height, 0]), // value -> display
+		yMap = function(d) { return yScale(yValue(d));}, // data -> display
+		yAxis = d3.axisLeft(yScale);
+
+	// setup fill color
+	var cValue = function(d) { return d[0] + d[1] * 10;},
+		color = d3.scaleOrdinal(d3.schemeCategory20);;
+
+	// add the graph canvas to the body of the webpage
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// add the tooltip area to the webpage
+	var tooltip = d3.select("body").append("div")
+		.attr("class", "tooltip")
+		.style("opacity", 0);
+
+	// load data
+	d3.csv(filename, function(error, data) {
+
+	  // change string (from CSV) into number format
+	  data.forEach(function(d) {
+		d[0] = +d[0];
+		d[1] = +d[1];
+	//    console.log(d);
+	  });
+
+	  // don't want dots overlapping axis, so add in buffer to data domain
+	  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+	  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+	  // x-axis
+	  svg.append("g")
+		  .attr("class", "x axis")
+		  .attr("transform", "translate(0," + height + ")")
+		  .call(xAxis)
+		.append("text")
+		  .attr("class", "label")
+		  .attr("x", width)
+		  .attr("y", -6)
+		  .style("text-anchor", "end")
+		  .text("Calories");
+
+	  // y-axis
+	  svg.append("g")
+		  .attr("class", "y axis")
+		  .call(yAxis)
+		.append("text")
+		  .attr("class", "label")
+		  .attr("transform", "rotate(-90)")
+		  .attr("y", 6)
+		  .attr("dy", ".71em")
+		  .style("text-anchor", "end")
+		  .text("Protein (g)");
+
+	  // draw dots
+	  svg.selectAll(".dot")
+		  .data(data)
+		.enter().append("circle")
+		  .attr("class", "dot")
+		  .attr("r", 3.5)
+		  .attr("cx", xMap)
+		  .attr("cy", yMap)
+		  .style("fill", function(d) { return color(cValue(d));})
+		  .on("mouseover", function(d) {
+			  tooltip.transition()
+				   .duration(200)
+				   .style("opacity", .9);
+			  tooltip.html(d["Cereal Name"] + "<br/> (" + xValue(d)
+				+ ", " + yValue(d) + ")")
+				   .style("left", (d3.event.pageX + 5) + "px")
+				   .style("top", (d3.event.pageY - 28) + "px");
+		  })
+		  .on("mouseout", function(d) {
+			  tooltip.transition()
+				   .duration(500)
+				   .style("opacity", 0);
+		  });
+
+	  // // draw legend
+	  // var legend = svg.selectAll(".legend")
+		//   .data(color.domain())
+		// .enter().append("g")
+		//   .attr("class", "legend")
+		//   .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      //
+	  // // draw legend colored rectangles
+	  // legend.append("rect")
+		//   .attr("x", width - 18)
+		//   .attr("width", 18)
+		//   .attr("height", 18)
+		//   .style("fill", color);
+      //
+	  // // draw legend text
+	  // legend.append("text")
+		//   .attr("x", width - 24)
+		//   .attr("y", 9)
+		//   .attr("dy", ".35em")
+		//   .style("text-anchor", "end")
+		//   .text(function(d) { return d;})
+	});
+}
+
+
+
+
