9aaa0ae768b7da599294584e20ff4b01f730b146
mukul2604 <mukul312@gmail.com>
2017-04-02 22:16:52 -0400

    js

--- static/js/main.js
+++ static/js/main.js
@@ -15,11 +15,8 @@ function chartTypeFunction() {
     document.getElementById("charttypes").classList.toggle("show");
 }
 
-var svg;
-
-function twoDScatterPlot(filename, id) {
-
-	var elements = document.getElementById("charttypes").children;
+function markSelection(id) {
+    var elements = document.getElementById("charttypes").children;
 	var i;
 
 	for (i=0; i < elements.length; i++) {
@@ -32,6 +29,14 @@ function twoDScatterPlot(filename, id) {
 			d3.select(selectElem).attr("class", "dropdown");
 		}
 	}
+}
+
+var svg;
+
+
+function twoDScatterPlot(filename, id) {
+
+	markSelection(id);
 
 	filename = "./static/data/" + filename;
 
@@ -40,10 +45,9 @@ function twoDScatterPlot(filename, id) {
         d3.selectAll('svg').remove();
     }
 
-	var margin = {top: 100, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
+    var margin = {top: 100, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
 	var xValue = function(d) { return d[0];}, // data -> value
 		xScale = d3.scaleLinear().range([0, width]), // value -> display
@@ -59,6 +63,8 @@ function twoDScatterPlot(filename, id) {
 
 	var cValue = function(d) { return d[0] + d[1] * 10;},
 		color = d3.scaleOrdinal(d3.schemeCategory20);
+
+
 
 	// add the graph canvas to the body of the webpage
 	svg = d3.select("body").append("svg")
@@ -158,4 +164,140 @@ function twoDScatterPlot(filename, id) {
 	});
 }
 
+function scatterPlotMatrix(filename, id) {
+    markSelection(id);
+
+	filename = "./static/data/" + filename;
+
+	if (svg != undefined) {
+        svg.selectAll('*').remove();
+        d3.selectAll('svg').remove();
+    }
+
+    var width = 960,
+        size = 250,
+        top_margin = 10,
+        padding = 20;
+
+    var x = d3.scaleLinear()
+        .range([padding / 2, size - padding / 2]);
+
+    var y = d3.scaleLinear()
+        .range([size - padding / 2, padding / 2]);
+
+    var xAxis = d3.axisBottom(x);
+
+    var yAxis = d3.axisLeft(x);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    // svg = d3.select("body").append("svg")
+		// .attr("width", width + margin.left + margin.right)
+		// .attr("height", height + margin.top + margin.bottom)
+	 //  	.append("g")
+		// .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv(filename, function (error, data) {
+        if (error) throw error;
+
+        var domainByTrait = {},
+            traits = d3.keys(data[0]).filter(function (d) {
+                return d !== "species";
+            }),
+            n = traits.length;
+
+        traits.forEach(function (trait) {
+            domainByTrait[trait] = d3.extent(data, function (d) {
+                return d[trait];
+            });
+        });
+
+        xAxis.tickSize(size * n);
+        yAxis.tickSize(-size * n);
+
+        svg = d3.select("body").append("svg")
+            .attr("width", size * n + padding)
+            .attr("height", size * n + padding)
+            .append("g")
+            .attr("transform", "translate(" + padding + "," + top_margin + ")");
+
+        svg.selectAll(".x.axis")
+            .data(traits)
+            .enter().append("g")
+            .attr("class", "x axis")
+            .attr("transform", function (d, i) {
+                return "translate(" + (n - i - 1) * size + ",0)";
+            })
+            .each(function (d) {
+                x.domain(domainByTrait[d]);
+                d3.select(this).call(xAxis);
+            });
+
+        svg.selectAll(".y.axis")
+            .data(traits)
+            .enter().append("g")
+            .attr("class", "y axis")
+            .attr("transform", function (d, i) {
+                return "translate(0," + i * size + ")";
+            })
+            .each(function (d) {
+                y.domain(domainByTrait[d]);
+                d3.select(this).call(yAxis);
+            });
+
+        var cell = svg.selectAll(".cell")
+            .data(cross(traits, traits))
+            .enter().append("g")
+            .attr("class", "cell")
+            .attr("transform", function (d) {
+                return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")";
+            })
+            .each(plot);
+
+        // Titles for the diagonal.
+        cell.filter(function (d) {
+            return d.i === d.j;
+        }).append("text")
+            .attr("x", padding)
+            .attr("y", padding)
+            .attr("dy", ".71em")
+            .text(function (d) {
+                return d.x;
+            });
+
+        function plot(p) {
+            var cell = d3.select(this);
+
+            x.domain(domainByTrait[p.x]);
+            y.domain(domainByTrait[p.y]);
+
+            cell.append("rect")
+                .attr("class", "frame")
+                .attr("x", padding / 2)
+                .attr("y", padding / 2)
+                .attr("width", size - padding)
+                .attr("height", size - padding);
+
+            cell.selectAll("circle")
+                .data(data)
+                .enter().append("circle")
+                .attr("cx", function (d) {
+                    return x(d[p.x]);
+                })
+                .attr("cy", function (d) {
+                    return y(d[p.y]);
+                })
+                .attr("r", 4)
+                .style("fill", function (d) {
+                    return color(d.species);
+                });
+        }
+    });
+}
+
+function cross(a, b) {
+  var c = [], n = a.length, m = b.length, i, j;
+  for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+  return c;
+}
 
